0da66be1a79ad14aa0d763802ed9a43b915f3a3b
Daniel Wang <kaiwang2027@gmail.com>
2026-05-04 17:14:43 +1000

    feat: remove standalone divider module

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { ExperienceSection } from './components/sections/ExperienceSection'
 import { HeroSection } from './components/sections/HeroSection'
 import { ProjectsSection } from './components/sections/ProjectsSection'
 import { ResumeUnavailableSection } from './components/sections/ResumeUnavailableSection'
-import { SignatureDivider } from './components/sections/SignatureDivider'
 import { TechStackSection } from './components/sections/TechStackSection'
 
 function App() {
   return (
     <PageShell>
       <HeroSection />
-      <SignatureDivider />
       <ExperienceSection />
       <TechStackSection />
       <ProjectsSection />
