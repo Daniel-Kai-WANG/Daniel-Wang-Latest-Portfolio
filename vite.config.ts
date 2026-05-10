01b5303f0e48458ac2dc1641ea6e537754790e34
Daniel Wang <kaiwang2027@gmail.com>
2026-05-11 04:36:47 +1000

    chore: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/Daniel-Wang-Latest-Portfolio/',
   plugins: [react()],
 })
