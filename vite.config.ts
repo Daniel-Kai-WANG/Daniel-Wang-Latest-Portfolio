f7f1b561d88b4d6f0c8b500389f17b91c25c4054
Daniel Wang <kaiwang2027@gmail.com>
2026-05-11 22:49:01 +1000

    fix: set correct github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/Daniel-Wang-Latest-Portfolio/'
+      : '/',
   plugins: [react()],
 })
